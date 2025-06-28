20bc4b5fa55bd70300805e31d1b0b118f036f0ed
longsq <longsqiang@gmail.com>
2025-06-28 10:24:50 +0800

    commit changes

---
+++ java-template/src/main/java/leetcode/editor/cn/LongestSubsequenceRepeatedKTimes.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+public class LongestSubsequenceRepeatedKTimes {
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestSubsequenceRepeatedK(String s, int k) {
+            
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    public static void main(String[] args) {
+        Solution solution = new LongestSubsequenceRepeatedKTimes().new Solution();
+        // put your test code here
+        
+    }
+}
